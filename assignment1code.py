93fb9595eeb1fe82f900c6cace6b087a715e32c5
Heemin Kang <heemin.kang@ucalgary.ca>
2024-05-09 09:54:17 -0600

    first commit

---
+++ assignment1code.py
@@ -0,0 +1,10 @@
+# ENSF 692 Spring 2024
+# Assignment 1 Starter Code
+# Heemin Kang
+
+studentName = "Heemin Kang"  # Change the contents of this variable to your name (leave the quotation marks).
+
+print("Hello, World!")
+print("My name is " + studentName + " and I am a student in ENSF 692.")
+
+print("my best skill is adaptability") # print your best skill here
